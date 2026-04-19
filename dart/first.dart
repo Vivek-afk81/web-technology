5df7ef7cca908c92f6ddd26d9fb5f29876e6eea6
Vivek-afk81 <vivekchauhan560999@gmail.com>
2026-04-19 11:05:04 +0530

    add initial Dart files with main function and commented functions

---
+++ dart/first.dart
@@ -0,0 +1,10 @@
+void main(){
+    var name="vivek";
+    int age=30;
+    double height=5.9;
+    bool isTeacher=true;
+
+    print("Name: $name");
+    print("Age: $age");
+    print("isTeacher: $isTeacher")
+}
---
+++ dart/functions/tempCodeRunnerFile.dart
@@ -0,0 +1,17 @@
+// void greet(){
+//     print("Hello Dart");
+
+// }
+// int add(int a,int b){
+//     return a+b;
+// }
+
+// void main(){
+//     greet(); // calling the function
+    
+//     int square(int n) => n*n; // Anonymous function
+//     print(square(5));
+
+//     int result=add(10,20);
+//     print(result);
+// }
